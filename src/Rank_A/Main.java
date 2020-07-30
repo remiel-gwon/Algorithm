91d6d151a2c4a98ba636d720073b5dcc0ff8884e
remiel-gwon <ramielbk56@icloud.com>
2020-07-30 17:27:15 +0900

    2020/07/30 commit

--- src/Rank_A/Main.java
+++ src/Rank_A/Main.java
@@ -2,6 +2,6 @@ package Rank_A;
 
 public class Main {
     public static void main(String[] args){
-        System.out.printf("Hello World");
+        System.out.println("test1");
     }
 }
--- src/Rank_B/Main.java
+++ src/Rank_B/Main.java
@@ -2,6 +2,6 @@ package Rank_B;
 
 public class Main {
     public static void main(String[] args){
-        System.out.printf("Hello World2");
+        System.out.println("test2");
     }
 }
